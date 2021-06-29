cab6fe3ac7e6936005401125881d4a48f6aa714f
huwdp <huw008@gmail.com>
2021-06-29 18:58:35 +0100

    Added links and improved web page, etc

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 
 # Flash Matrix
-The matrix gives approximate support of [Shumway](https://mozilla.github.io/shumway/), [Lightspark](https://lightspark.github.io/) and [Gnash](https://www.gnu.org/software/gnash/) flash players compared the defacto standard Adobe Flash Player. The matrix is not 100% correct due to the difficulty in measuring API support within the source code of each project. 
+The matrix gives approximate support of Shumway, Lightspark, Gnash and Ruffle flash players compared the defacto standard Adobe Flash Player. The matrix is not 100% correct due to the difficulty in measuring API support within the source code of each project.
 
-The matrix can be found here [here](https://huwdp.github.io/Flash-Matrix/)  (https://huwdp.github.io/Flash-Matrix/)
-
-Adobe Flash support will end on December 31, 2020. Click [here](https://www.adobe.com/products/flashplayer/end-of-life.html#) for information about the end of life support details.
-
-
+Adobe Flash support ended on December 31, 2020. Click here for information about the end of life support details.
 
 ## Getting Started
 
@@ -17,6 +13,7 @@ sudo apt-get install python3 git
 git clone https://github.com/mozilla/shumway.git
 git clone https://github.com/strk/gnash.git
 git clone https://github.com/lightspark/lightspark.git
+git clone https://github.com/ruffle-rs/ruffle.git
 ```
 
 ### Inatall
@@ -35,11 +32,6 @@ python3 build-matrix.py
 ## License
 
 This project is licensed under the GPL3 license - see the [LICENSE](LICENSE) file for details.
-
-## Todo
- 1. Grab every method in every class from Adobe's AS3 documentation.
- 2. Check whether methods exist in code and check whether there are no log methods to see if they are not implemented. So if the method exists then set feature to true. Then if there is a log method for the method then set the feature to false.
- 3. Add other Flash implementations
 
 ## Thanks
 I'd like to link to Mesa Matrix [here](https://github.com/MightyCreak/mesamatrix) (https://github.com/MightyCreak/mesamatrix). Flash Matrix was created with inspiration from Mesa Matrix, so thanks!
---
+++ page.js
@@ -0,0 +1,93 @@
+var flashPlayers = ["adobeflash", "lightspark", "shumway", "ruffle", "gnash"];
+var container = document.getElementById("flash-matrix");
+var statsContainer = document.getElementById("stats");
+
+function replaceNames(name) {
+    return name.replace("adobeflash", "Adobe Flash");
+}
+
+fetch('flash-matrix.json')
+    .then(response => response.text())
+    .then((data) => {
+        var json = JSON.parse(data);
+        var modifiedOn = json['modifiedOn'];
+        var matrix = json['matrix'];
+
+        document.getElementById('modifiedOn').innerHTML = "Last modified on " + modifiedOn;
+
+        var statsTable = document.createElement("table");
+        var featureTotalCount = 0;
+        for (category in matrix) {
+            for (feature in matrix[category]) {
+                featureTotalCount++;
+            }
+        }
+
+        for (player in flashPlayers) {
+            var playerFeatureCount = 0;
+            for (category in matrix) {
+                for (feature in matrix[category]) {
+                    if (matrix[category][feature][flashPlayers[player]] == "Yes") {
+                        playerFeatureCount++;
+                    }
+                }
+            }
+            var playerStatsTr = document.createElement("tr");
+            var playerName = document.createElement("td");
+            playerName.innerHTML = replaceNames(flashPlayers[player]);
+            var playerSupport = document.createElement("td");
+            playerSupport.innerHTML = playerFeatureCount + " (" + (playerFeatureCount / 328 * 100).toFixed(2) + "%)";
+            playerStatsTr.appendChild(playerName);
+            playerStatsTr.appendChild(playerSupport);
+            statsTable.appendChild(playerStatsTr);
+        }
+        statsContainer.appendChild(statsTable);
+
+        for (var category in matrix) {
+            var table = document.createElement("table");
+
+            var header = document.createElement("tr");
+            var blankHeaderItem = document.createElement("th");
+            blankHeaderItem.innerHTML = "<strong>" + category + "</strong>";
+            header.appendChild(blankHeaderItem);
+            for (player in flashPlayers) {
+                var playerTd = document.createElement("th");
+                playerTd.innerHTML = replaceNames(flashPlayers[player]);
+                header.appendChild(playerTd);
+            }
+            table.appendChild(header);
+
+            for (feature in matrix[category]) {
+                var tr = document.createElement("tr");
+
+                var featureTd = document.createElement("td");
+
+                var featureUrl = '<a class="featureUrl" href="https://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/flash/' + category + '/' + feature + '.html">' + feature + '</a>';
+                featureTd.innerHTML = featureUrl;
+                featureTd.style.width = "300px";
+                tr.appendChild(featureTd);
+
+                for (player in flashPlayers) {
+                    var td = document.createElement("td");
+
+                    var featureResult = matrix[category][feature][flashPlayers[player]];
+                    td.innerHTML = featureResult;
+
+                    if (featureResult == "Yes") {
+                        td.style.backgroundColor = "green";
+                    } else if (featureResult == "No") {
+                        td.style.backgroundColor = "orange";
+                    } else {
+                        td.style.backgroundColor = "yellow";
+                    }
+
+                    tr.appendChild(td);
+                }
+                table.appendChild(tr);
+            }
+
+            container.appendChild(table);
+            var breakLine = document.createElement("br");
+            container.appendChild(breakLine);
+        }
+    });
