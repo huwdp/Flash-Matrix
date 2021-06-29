075e1a8ae63a02752199c561c2734ee333f45a70
huwdp <huw008@gmail.com>
2021-06-29 18:31:47 +0100

    Slightly improve quality of page/codebase

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# Todo
- 1. Grab every method in every class from Adobe's AS3 documentation.
- 2. Check whether methods exist in code and check whether there are no log methods to see if they are not implemented. So if the method exists then set feature to true. Then if there is a log method for the method then set the feature to false.
--- buildtable.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/lightspark/lightspark.git
-git clone https://github.com/mozilla/shumway.git
-git clone https://github.com/strk/gnash.git
-
-
-git pull https://github.com/lightspark/lightspark.git
-git pull https://github.com/mozilla/shumway.git
-git clone https://github.com/strk/gnash.git
-python buildtable.py
